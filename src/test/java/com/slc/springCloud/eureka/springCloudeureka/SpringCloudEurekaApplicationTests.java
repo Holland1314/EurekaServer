1df09c45283e25d9b291f391d4cea3ae32ad6493
shileichao <shileichao@auto-link.com.cn>
2018-05-23 17:36:00 +0800

    init

---
+++ src/test/java/com/slc/springCloud/eureka/springCloudeureka/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.slc.springCloud.eureka.springCloudeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
