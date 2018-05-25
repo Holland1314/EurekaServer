9c7ea9a47ed1415d3739493edc42bb0ffda56a41
shileichao <shileichao@auto-link.local>
2018-05-25 17:42:26 +0800

    remove security authority

--- src/main/java/com/slc/springCloud/eureka/springCloudeureka/SpringCloudEurekaApplication.java
+++ src/main/java/com/slc/springCloud/eureka/springCloudeureka/SpringCloudEurekaApplication.java
@@ -8,7 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
 
+/*	org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter
+	 org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+	org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter*/
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringCloudEurekaApplication.class, args);
 	}
 }
